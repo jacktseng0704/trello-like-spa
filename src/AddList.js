a9605cd397af5de523fb208c0e7c91f24741b6a1
Jack Tseng <b99901190@gmail.com>
2020-11-20 11:50:20 +0800

    Created List Component

--- src/AddList.js
+++ src/AddList.js
@@ -1,17 +1,29 @@
 import React, { useState } from 'react';
 import Form from './Form';
 import './AddList.css';
+import List from './List';
 
 export default function AddList() {
+  const [lists, setLists] = useState([]);
+  const addList = (title) => {
+    setLists([...lists, title]);
+    console.log(title);
+  };
   const [showForm, setShowForm] = useState(false);
   const onClick = () => setShowForm(true);
 
   return (
     <div className='AddListComponent'>
-      <button onClick={onClick}>+ Add a list</button>
-      {showForm ? (
-        <Form className='HiddenForm' setShowForm={setShowForm} showForm={showForm} />
-      ) : null}
+      {lists.map((list) => (
+        <List title={list} />
+      ))}
+      <div className='ListComponent'>
+        {showForm ? (
+          <Form className='HiddenForm' setShowForm={setShowForm} addList={addList} />
+        ) : (
+          <button onClick={onClick}>+ Add a list</button>
+        )}
+      </div>
     </div>
   );
 }
--- src/Form.js
+++ src/Form.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Form.css';
 
 export default function Form(props) {
-  console.log(props);
+  // console.log(props);
+  const [title, setTitle] = useState('');
 
   function handleClick(event) {
     props.setShowForm(false);
   }
 
+  function handleInputChange(event) {
+    setTitle(event.target.value);
+  }
+
+  function addList() {
+    if (title) {
+      props.addList(title);
+    }
+    setTitle('');
+  }
+
   return (
     <div className='MyForm'>
-      <input />
-      <button onClick={handleClick}> X </button>
+      <input onChange={handleInputChange} value={title} />
+      <div className='MyButtons'>
+        <button onClick={addList}>Add List</button>
+        <button onClick={handleClick}> X </button>
+      </div>
     </div>
   );
 }
